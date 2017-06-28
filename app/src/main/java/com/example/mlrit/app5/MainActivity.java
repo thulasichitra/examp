53062a7a412a1b8952dd3ab70a7b5a45f62edcee
thulasichitra <thulasichitra@gmail.com>
2017-06-28 15:40:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mlrit/app5/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mlrit.app5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        b1=(Button)findViewById(R.id.b1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this,"welcome to Mlrit",Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
